21524a4be1261b11d2dc39b317b3967e675f4930
Abidluayraihantaufik <spaskeren30@gmail.com>
2023-12-18 19:29:46 +0700

    first

---
+++ config.php
@@ -0,0 +1,7 @@
+    private function getConfig()
+    {
+        $this->host = "localhost";
+        $this->user = "root";
+        $this->password = "";
+        $this->db_name = "latihan1";
+    }
